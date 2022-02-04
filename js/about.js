d473969e9c57eec699247512b5cb874d97525f11
T-Al-D <tahaaldailami2010@hotmail.de>
2022-02-04 21:13:05 +0100

    added mostly CSS, a bit HTML, little JavaScript

--- js/about.js
+++ js/about.js
@@ -1,3 +1,11 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+	// display data to the images
+	const images = document.getElementsByClassName("flower_images");
+	for (let image of images) {
+		image.addEventListener("click", () => {
+			alert(image.dataset.info);
+		});
+	}
+});
