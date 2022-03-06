a5749c5abbfc8fddbeacab4ab45a9b48d778c9d5
T-Al-D <tahaaldailami2010@hotmail.de>
2022-03-06 18:00:28 +0100

    changed index added conversion block

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ My learning experiences with web development.
 This project is my first try at web development.
 The goal here is only to try out different desins and get in touch with HTML, CSS and JavaScript.
 
-Here is it as a webpage: https://t-al-d.github.io/WebExample/
+Here is it as a WEBPAGE: https://t-al-d.github.io/WebExample/
 
 All picture provided here, are from me (not a website) .
+
+ICO-Contverter: 	  https://image.online-convert.com/convert-to-ico
+Pixelart (icon of index): https://www.pixilart.com/
--- js/index.js
+++ js/index.js
@@ -2,12 +2,41 @@
 
 // after the page (Document-Object-Model) loaded do ...
 document.addEventListener("DOMContentLoaded", () => {
-    // text INPUT
-    const textInput = document.getElementById("textInput");
-    const displayTextButton = document.getElementById("textInputButton");
-    displayTextButton.addEventListener("click", () => {
-        const textDisplay = document.getElementById("textDisplay");
-        textDisplay.textContent = textInput.value;
-        textInput.value = "";
-    });
+	// text INPUT
+	const textInput = document.getElementById("textInput");
+	const displayTextButton = document.getElementById("textInputButton");
+	displayTextButton.addEventListener("click", () => {
+		const textDisplay = document.getElementById("textDisplay");
+		textDisplay.textContent = textInput.value;
+		textInput.value = "";
+	});
+	// conversion
+	const numberInput = document.getElementById("numberInput");
+	const numberButton = document.getElementById("numberButton");
+	numberButton.addEventListener("click", () => {
+		const resultDisplay = document.getElementById("resultDisplay");
+		const radioButtons = document.getElementsByName("conversion");
+		let messageString = "RESULT: " + numberInput.value + " ";
+		for (const radioButton of radioButtons) {
+			if (radioButton.checked) {
+				if (radioButton.id === "internetSpeed") {
+					messageString +=
+						"MBits/s equals to " +
+						Math.round((numberInput.value / 8) * 100) / 100 +
+						"\n" +
+						" MB/s, but usually MB is only used for storage units!";
+				}
+				if (radioButton.id === "storageSize") {
+					messageString +=
+						"GB (claimed) equals to " +
+						Math.round(numberInput.value * 0.931 * 100) / 100 +
+						"\n" +
+						" real GB in storage, because it is advertised as GigaByte (1000^3 Bytes)" +
+						" but actually the computer measures in Gibibyte (1024^3 Bytes)! ";
+				}
+			}
+		}
+		resultDisplay.textContent = messageString;
+		numberInput.value = "";
+	});
 });
